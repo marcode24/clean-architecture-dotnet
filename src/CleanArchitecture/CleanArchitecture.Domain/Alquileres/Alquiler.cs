24594171a2309f07c11278009ea5fe9d8daa640d
Marco Cruz <marco24cruz08@gmail.com>
2024-07-31 22:08:32 -0600

    ✨ Add Alquiler domain model and related classes

---
+++ src/CleanArchitecture/CleanArchitecture.Domain/Alquileres/Alquiler.cs
@@ -0,0 +1,70 @@
+using CleanArchitecture.Domain.Abstractions;
+using CleanArchitecture.Domain.Alquileres.Events;
+using CleanArchitecture.Domain.Vehiculos;
+
+namespace CleanArchitecture.Domain.Alquileres;
+
+public sealed class Alquiler : Entity
+{
+  private Alquiler(
+    Guid id,
+    Guid vehiculoId,
+    Guid usuarioId,
+    DateRange duracion,
+    Moneda precioPorPeriodo,
+    Moneda mantenimiento,
+    Moneda accesorios,
+    Moneda precioTotal,
+    AlquilerStatus status,
+    DateTime fechaCreacion
+  ) : base(id)
+  {
+    VehiculoId = vehiculoId;
+    UsuarioId = usuarioId;
+    PrecioPorPeriodo = precioPorPeriodo;
+    Mantenimiento = mantenimiento;
+    Accesorios = accesorios;
+    PrecioTotal = precioTotal;
+    Status = status;
+    Duracion = duracion;
+    FechaCreacion = fechaCreacion;
+  }
+  public Guid VehiculoId { get; private set; }
+  public Guid UsuarioId { get; private set; }
+  public Moneda? PrecioPorPeriodo { get; private set; }
+  public Moneda? Mantenimiento { get; private set; }
+  public Moneda? Accesorios { get; private set; }
+  public Moneda? PrecioTotal { get; private set; }
+  public AlquilerStatus Status { get; private set; }
+  public DateRange? Duracion { get; private set; }
+  public DateTime? FechaCreacion { get; private set; }
+  public DateTime? FechaConfirmacion { get; private set; }
+  public DateTime? FechaDenegacion { get; private set; }
+  public DateTime? FechaCompletado { get; private set; }
+  public DateTime? FechaCancelacion { get; private set; }
+  public static Alquiler Reservar(
+    Guid vehiculoId,
+    Guid usuarioId,
+    DateRange duracion,
+    DateTime fechaCreacion,
+    PrecioDetalle precioDetalle
+  )
+  {
+    var alquiler = new Alquiler(
+      Guid.NewGuid(),
+      vehiculoId,
+      usuarioId,
+      duracion,
+      precioDetalle.PrecioPorPeriodo,
+      precioDetalle.Mantenimiento,
+      precioDetalle.Accesorios,
+      precioDetalle.PrecioTotal,
+      AlquilerStatus.Reservado,
+      fechaCreacion
+    );
+
+    alquiler.RaiseDomainEvent(new AlquilerReservadoDomainEvent(alquiler.Id!));
+
+    return alquiler;
+  }
+}
---
+++ src/CleanArchitecture/CleanArchitecture.Domain/Alquileres/DateRange.cs
@@ -0,0 +1,23 @@
+namespace CleanArchitecture.Domain.Alquileres;
+
+public sealed record DateRange
+{
+  private DateRange()
+  {
+
+  }
+
+  public DateOnly Inicio { get; init; }
+  public DateOnly Fin { get; init; }
+  public int CantidadDias => Fin.DayNumber - Inicio.DayNumber;
+  public static DateRange Create(DateOnly inicio, DateOnly fin)
+  {
+    if (inicio > fin) throw new ApplicationException("La fecha de inicio no puede ser mayor a la fecha de fin");
+
+    return new DateRange
+    {
+      Inicio = inicio,
+      Fin = fin
+    };
+  }
+}
