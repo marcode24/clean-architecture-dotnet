668026dfb5722e282eee4ff097fa61df69823026
Marco Cruz <marco24cruz08@gmail.com>
2024-08-03 17:29:43 -0600

    ✨ Add DateOnlyTypeHandler and SqlConnectionFactory classes

--- src/CleanArchitecture/CleanArchitecture.Infrastructure/DependencyInjection.cs
+++ src/CleanArchitecture/CleanArchitecture.Infrastructure/DependencyInjection.cs
@@ -1,12 +1,15 @@
 using CleanArchitecture.Application.Abstractions.Clock;
+using CleanArchitecture.Application.Abstractions.Data;
 using CleanArchitecture.Application.Abstractions.Email;
 using CleanArchitecture.Domain.Abstractions;
 using CleanArchitecture.Domain.Alquileres;
 using CleanArchitecture.Domain.Users;
 using CleanArchitecture.Domain.Vehiculos;
 using CleanArchitecture.Infrastructure.Clock;
+using CleanArchitecture.Infrastructure.Data;
 using CleanArchitecture.Infrastructure.Email;
 using CleanArchitecture.Infrastructure.Repositories;
+using Dapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -36,6 +39,10 @@ public static class DependencyInjection
     services.AddScoped<IAlquilerRepository, AlquilerRepository>();
 
     services.AddScoped<IUnitOfWork, IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+    services.AddSingleton<ISqlConnectionFactory>(_ => new SqlConnectionFactory(connectionString));
+
+    SqlMapper.AddTypeHandler(new DateOnlyTypeHandler());
 
     return services;
   }
