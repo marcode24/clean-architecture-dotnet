a4706e4ac3a61e540cde19b77883d856eb8d7d32
Marco Cruz <marco24cruz08@gmail.com>
2024-08-03 19:51:05 -0600

    ✨ Add CleanArchitecture.Infrastructure and CleanArchitecture.Application project references to CleanArchitecture.Api.csproj

--- src/CleanArchitecture/CleanArchitecture.Api/Program.cs
+++ src/CleanArchitecture/CleanArchitecture.Api/Program.cs
@@ -1,9 +1,15 @@
+using CleanArchitecture.Application;
+using CleanArchitecture.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
