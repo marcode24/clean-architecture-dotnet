ee0dbc9ed1e30424513354f9d1ffaaf0797be5fa
Marco Cruz <marco24cruz08@gmail.com>
2024-08-08 18:29:11 -0600

    ✨ Add PaginationResult and SpecificationEntry classes

---
+++ src/CleanArchitecture/CleanArchitecture.Domain/Shared/SpecificationEntry.cs
@@ -0,0 +1,18 @@
+namespace CleanArchitecture.Domain.Shared;
+
+
+public record SpecificationEntry
+{
+  private const int MaxPageSize = 50;
+  private int _pageSize = 20;
+
+  public string? Sort { get; set; }
+  public int PageIndex { get; set; } = 1;
+  public int PageSize
+  {
+    get => _pageSize;
+    set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+  }
+  public string? Search { get; set; }
+  
+}
