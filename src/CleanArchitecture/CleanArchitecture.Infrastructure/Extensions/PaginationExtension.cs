422f3a0bbc973e0da081709f96228c8adff942be
Marco Cruz <marco24cruz08@gmail.com>
2024-08-10 21:55:17 -0600

    ✨ Add PaginationExtension class for ordering queryable by property or field

---
+++ src/CleanArchitecture/CleanArchitecture.Infrastructure/Extensions/PaginationExtension.cs
@@ -0,0 +1,29 @@
+using System.Linq.Expressions;
+
+namespace CleanArchitecture.Infrastructure.Extensions;
+
+public static class PaginationExtension
+{
+  public static IQueryable<T> OrderByPropertyOfField<T>(
+    this IQueryable<T> queryable,
+    string propertyOrFieldName,
+    bool ascending = true
+  )
+  {
+    var elementType = typeof(T);
+    var orderByMethodName = ascending ? "OrderBy" : "OrderByDescending";
+    var parameterExpression = Expression.Parameter(elementType);
+    var propertyOrFieldExpression = Expression.PropertyOrField(parameterExpression, propertyOrFieldName);
+
+    var selector = Expression.Lambda(propertyOrFieldExpression, parameterExpression);
+    var orderByExpression = Expression.Call(
+      typeof(Queryable),
+      orderByMethodName,
+      [elementType, propertyOrFieldExpression.Type],
+      queryable.Expression,
+      selector
+    );
+
+    return queryable.Provider.CreateQuery<T>(orderByExpression);
+  }
+}
--- src/CleanArchitecture/CleanArchitecture.Infrastructure/Repositories/Repository.cs
+++ src/CleanArchitecture/CleanArchitecture.Infrastructure/Repositories/Repository.cs
@@ -1,5 +1,6 @@
 using System.Linq.Expressions;
 using CleanArchitecture.Domain.Abstractions;
+using CleanArchitecture.Infrastructure.Extensions;
 using CleanArchitecture.Infrastructure.Specifications;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -15,7 +16,7 @@ internal abstract class Repository<TEntity, TEntityId> where TEntity : Entity<TE
     _dbContext = dbContext;
   }
 
-  public async Task<TEntity> GetByIdAsync(TEntityId id, CancellationToken cancellationToken = default)
+  public async Task<TEntity?> GetByIdAsync(TEntityId id, CancellationToken cancellationToken = default)
   {
     return await _dbContext.Set<TEntity>()
       .FirstOrDefaultAsync(e => e.Id == id, cancellationToken);
@@ -46,7 +47,7 @@ internal abstract class Repository<TEntity, TEntityId> where TEntity : Entity<TE
     return await ApplySpecification(specification).CountAsync(cancellationToken);
   }
 
-  public async Task<PagedResults<TEntity, TEntityId> GetPaginationAsync(
+  public async Task<PagedResults<TEntity, TEntityId>> GetPaginationAsync(
     Expression<Func<TEntity, bool>>? predicate,
     Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes,
     int page,
@@ -54,5 +55,45 @@ internal abstract class Repository<TEntity, TEntityId> where TEntity : Entity<TE
     string orderBy,
     bool ascending,
     bool disableTracking = true
-  ) {}
+  )
+  {
+    IQueryable<TEntity> queryable = _dbContext.Set<TEntity>();
+
+    if (disableTracking)
+      queryable = queryable.AsNoTracking();
+
+    if (predicate is not null)
+      queryable = queryable.Where(predicate);
+
+    if (includes is not null)
+      queryable = includes(queryable);
+
+    var skipAmount = pageSize * (page - 1);
+    var totalNumberOfRecords = await queryable.CountAsync();
+    var records = new List<TEntity>();
+
+    if (string.IsNullOrEmpty(orderBy))
+      records = await queryable
+                      .Skip(skipAmount)
+                      .Take(pageSize)
+                      .ToListAsync();
+    else
+      records = await queryable
+                      .OrderByPropertyOfField(orderBy, ascending)
+                      .Skip(skipAmount)
+                      .Take(pageSize)
+                      .ToListAsync();
+
+    var mod = totalNumberOfRecords % pageSize;
+    var totalPageCount = (totalNumberOfRecords / pageSize) + (mod == 0 ? 0 : 1);
+
+    return new PagedResults<TEntity, TEntityId>
+    {
+      Results = records,
+      PageNumber = page,
+      PageSize = pageSize,
+      TotalNumberOfPages = totalPageCount,
+      TotalNumberOfRecords = totalNumberOfRecords
+    };
+  }
 }
