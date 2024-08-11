15a0e48e6f1f575e714d5b521da57d20f755fd67
Marco Cruz <marco24cruz08@gmail.com>
2024-08-11 15:43:15 -0600

    🧪Update project structure and dependencies for unit testing

--- src/CleanArchitecture/CleanArchitecture.Infrastructure/Repositories/VehiculoRepository.cs
+++ src/CleanArchitecture/CleanArchitecture.Infrastructure/Repositories/VehiculoRepository.cs
@@ -1,3 +1,4 @@
+using CleanArchitecture.Domain.Abstractions;
 using CleanArchitecture.Domain.Vehiculos;
 
 namespace CleanArchitecture.Infrastructure.Repositories;
@@ -5,4 +6,14 @@ namespace CleanArchitecture.Infrastructure.Repositories;
 internal sealed class VehicleRepository : Repository<Vehiculo, VehiculoId>, IVehiculosRepository
 {
   public VehicleRepository(ApplicationDbContext dbContext) : base(dbContext) { }
+
+  public Task<int> CountAsync(ISpecification<Vehiculo, VehiculoId> spec)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IReadOnlyList<Vehiculo>> GetAllWithSpec(ISpecification<Vehiculo, VehiculoId> spec)
+  {
+    throw new NotImplementedException();
+  }
 }
