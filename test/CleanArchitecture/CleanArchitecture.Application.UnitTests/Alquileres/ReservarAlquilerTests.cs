603f62e473fb2016895a05c3752cb17bf8dfa66c
Marco Cruz <marco24cruz08@gmail.com>
2024-08-23 00:38:47 -0600

    🧪 Add InternalsVisibleTo attribute for unit testing

---
+++ test/CleanArchitecture/CleanArchitecture.Application.UnitTests/Alquileres/ReservarAlquilerTests.cs
@@ -0,0 +1,45 @@
+using CleanArchitecture.Application.Abstractions.Clock;
+using CleanArchitecture.Application.Alquileres.ReservarAlquiler;
+using CleanArchitecture.Domain.Abstractions;
+using CleanArchitecture.Domain.Alquileres;
+using CleanArchitecture.Domain.Users;
+using CleanArchitecture.Domain.Vehiculos;
+using NSubstitute;
+
+namespace CleanArchitecture.Application.UnitTests.Alquileres;
+
+public class ReservarAlquilerTests
+{
+  private readonly ReservarAlquilerCommandHandler _reservarAlquilerCommandHandlerMock;
+  private readonly IUserRepository _userRepositoryMock;
+  private readonly IVehiculosRepository _vehiculosRepositoryMock;
+  private readonly IAlquilerRepository _alquilerRepositoryMock;
+  private readonly IUnitOfWork _unitOfWorkMock;
+  private readonly DateTime UtcNow = DateTime.UtcNow;
+  private readonly ReservarAlquilerCommand _reservarAlquilerCommand = new(
+    new VehiculoId(Guid.NewGuid()),
+    new UserId(Guid.NewGuid()),
+    new DateOnly(2024, 1, 1),
+    new DateOnly(2025, 1, 2)
+  );
+
+  public ReservarAlquilerTests()
+  {
+    _userRepositoryMock = Substitute.For<IUserRepository>();
+    _vehiculosRepositoryMock = Substitute.For<IVehiculosRepository>();
+    _alquilerRepositoryMock = Substitute.For<IAlquilerRepository>();
+    _unitOfWorkMock = Substitute.For<IUnitOfWork>();
+
+    IDateTimeProvider dateTimeProviderMock = Substitute.For<IDateTimeProvider>();
+    dateTimeProviderMock.currentTime.Returns(UtcNow);
+
+    _reservarAlquilerCommandHandlerMock = new ReservarAlquilerCommandHandler(
+      _userRepositoryMock,
+      _vehiculosRepositoryMock,
+      _alquilerRepositoryMock,
+      new PrecioService(),
+      _unitOfWorkMock,
+      dateTimeProviderMock
+    );
+  }
+}
